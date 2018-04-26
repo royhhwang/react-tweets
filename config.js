75c0136f655efc4320591fd2c0744155e533c89f
Roy Hwang <royhhwang@gmail.com>
2018-04-25 21:14:12 -0700

    react twitter tutorial

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   twitter: {
-    consumer_key: 'MY_CONSUMER_KEY',
-    consumer_secret: 'MY_CONSUMER_SECRET',
-    access_token_key: 'MY_ACCESS_TOKEN_KEY',
-    access_token_secret: 'MY_ACCESS_TOKEN_SECRET'
+    consumer_key: 'EETaCkow1p1jfLRjoZuo8Ae2u',
+    consumer_secret: 'aM4uqn8JWfVKwiObYp1DEORSAPgJzVrQV6nxV8h4s6mig4CiTH',
+    access_token_key: '942987061216256001-q5b6F5LosTElbjaPX5QAZzhSniC7mW3',
+    access_token_secret: 'puDEzlmO7CAGCSi6jkNuTqpsqbBvZsqhsQTr3uImmcmXt'
   }
 }
