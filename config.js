5340f1651517f008d2ad5953a03ba712585cc31a
Roy Hwang <royhhwang@gmail.com>
2018-04-25 21:17:32 -0700

    removed config

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  twitter: {
-    consumer_key: 'EETaCkow1p1jfLRjoZuo8Ae2u',
-    consumer_secret: 'aM4uqn8JWfVKwiObYp1DEORSAPgJzVrQV6nxV8h4s6mig4CiTH',
-    access_token_key: '942987061216256001-q5b6F5LosTElbjaPX5QAZzhSniC7mW3',
-    access_token_secret: 'puDEzlmO7CAGCSi6jkNuTqpsqbBvZsqhsQTr3uImmcmXt'
-  }
-}
